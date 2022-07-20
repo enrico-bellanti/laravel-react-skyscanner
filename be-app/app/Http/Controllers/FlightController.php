9d88eef7445683acae5075788c90164a35029088
enrico bellanti <67062039+enrico-bellanti@users.noreply.github.com>
2022-07-20 11:28:32 +0200

    be api filter search

--- be-app/app/Http/Controllers/FlightController.php
+++ be-app/app/Http/Controllers/FlightController.php
@@ -22,43 +22,77 @@ class FlightController extends Controller
             'code_arrival' => 'required|string|exists:airports,code|max:3',
         ]);
 
-        // $per_page = $request->get('per_page', 25);
-        // $sort = $request->get('sort', 'price');
-        // $order = $request->get('order', 'DESC');
-
         $dep_code = $validated['code_departure'];
         $arr_code = $validated['code_arrival'];
 
         // NO STEPOVER
-        // $flight_list['STEPOVER_0'] = DB::table('flights')
-        //     ->where($validated)
-        //     ->get();
+        $flight_list['STEPOVER_0'] = DB::table('flights')
+            ->where($validated)
+            ->select(
+                'id AS id_1',
+                'code_departure AS code_departure_1',
+                'code_arrival AS code_arrival_1',
+                'price AS price_1',
+                'created_at AS created_at_1',
+                'updated_at AS updated_at_1',
+            )
+            ->get();
 
         // 1 STEOVER
         $flight_list['STEPOVER_1'] = DB::table('flights AS FLT1')
             ->where('FLT1.code_departure', $dep_code)
             ->whereNot('FLT1.code_arrival', $arr_code)
             ->join('flights AS FLT2', 'FLT1.code_arrival', '=', 'FLT2.code_departure')
+            ->whereNot('FLT2.code_departure', $dep_code)
             ->where('FLT2.code_arrival', $arr_code)
-            ->select('FLT1.*', 'FLT2.*')
+            ->select(
+                'FLT1.id AS id_1',
+                'FLT1.code_departure AS code_departure_1',
+                'FLT1.code_arrival AS code_arrival_1',
+                'FLT1.price AS price_1',
+                'FLT1.created_at AS created_at_1',
+                'FLT1.updated_at AS updated_at_1',
+                'FLT2.id AS id_2',
+                'FLT2.code_departure AS code_departure_2',
+                'FLT2.code_arrival AS code_arrival_2',
+                'FLT2.price AS price_2',
+                'FLT2.created_at AS created_at_2',
+                'FLT2.updated_at AS updated_at_2'
+            )
             ->get();
 
         // 2 STEOVER
-        // $flight_list['STEPOVER_2'] = DB::table('flights AS FLT1')
-        //     ->where('FLT1.code_departure', $dep_code)
-        //     ->whereNot('FLT1.code_arrival', $arr_code)
-        //     ->join('flights AS FLT2', 'FLT1.code_arrival', '=', 'FLT2.code_departure')
-        //     ->whereNot('FLT2.code_departure', $dep_code)
-        //     ->whereNot('FLT2.code_arrival', $arr_code)
-        //     ->join('flights AS FLT3', 'FLT2.code_arrival', '=', 'FLT3.code_departure')
-        //     ->where('FLT3.code_arrival', $arr_code)
-        //     ->get();
+        $flight_list['STEPOVER_2'] = DB::table('flights AS FLT1')
+            ->where('FLT1.code_departure', $dep_code)
+            ->whereNot('FLT1.code_arrival', $arr_code)
+            ->join('flights AS FLT2', 'FLT1.code_arrival', '=', 'FLT2.code_departure')
+            ->whereNot('FLT2.code_departure', $dep_code)
+            ->whereNot('FLT2.code_arrival', $arr_code)
+            ->join('flights AS FLT3', 'FLT2.code_arrival', '=', 'FLT3.code_departure')
+            ->whereNot('FLT3.code_departure', $dep_code)
+            ->where('FLT3.code_arrival', $arr_code)
+            ->select(
+                'FLT1.id AS id_1',
+                'FLT1.code_departure AS code_departure_1',
+                'FLT1.code_arrival AS code_arrival_1',
+                'FLT1.price AS price_1',
+                'FLT1.created_at AS created_at_1',
+                'FLT1.updated_at AS updated_at_1',
+                'FLT2.id AS id_2',
+                'FLT2.code_departure AS code_departure_2',
+                'FLT2.code_arrival AS code_arrival_2',
+                'FLT2.price AS price_2',
+                'FLT2.created_at AS created_at_2',
+                'FLT2.updated_at AS updated_at_2',
+                'FLT3.id AS id_3',
+                'FLT3.code_departure AS code_departure_3',
+                'FLT3.code_arrival AS code_arrival_3',
+                'FLT3.price AS price_3',
+                'FLT3.created_at AS created_at_3',
+                'FLT3.updated_at AS updated_at_3'
+            )
+            ->get();
 
-        dd($flight_list);
-
-
-        // $flights = $query->orderBy($sort, $order)->paginate($per_page);
-
-        // return response()->json($flights);
+        return response()->json($flight_list);
     }
 }
